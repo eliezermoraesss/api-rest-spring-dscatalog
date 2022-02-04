88bfb969fac3aef7459189cc78db6be56ada6172
eliezer <eliezer.moraes@outlook.com>
2022-02-04 17:14:24 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (NAME) VALUES ('Books');
-INSERT INTO tb_category (NAME) VALUES ('Electronics');
-INSERT INTO tb_category (NAME) VALUES ('Cars');
+INSERT INTO tb_category (NAME, created_At) VALUES ('Books', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Electronics', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Cars', NOW());
