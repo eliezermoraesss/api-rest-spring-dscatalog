7b6a3fce72a35e2828ef62cf5a43b4c370e2ff1c
eliezermoraesss <eliezer.moraes@outlook.com>
2022-07-11 13:51:01 -0300

    Navbar styling

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,14 +1,31 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
 const Navbar = () => {
   return (
-    <nav className="bg-primary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary  main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
         </a>
-        <div>
-          <ul>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscatalog-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">HOME</a>
+              <a href="link" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
