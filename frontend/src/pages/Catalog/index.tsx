27dd744720d45ae6e1639237fecb8ec466ccca14
eliezer <eliezer.moraes@outlook.com>
2022-12-05 11:32:41 -0300

    catalog title

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import './styles.css';
 
 const Catalog = () => {
 
@@ -24,7 +25,10 @@ const Catalog = () => {
   }
 
   return (
-    <div className="container my-4">
+    <div className="container my-4 catalog-container">
+      <div className='row catalog-title-container'>
+        <h1>Catálogo de produtos</h1>
+      </div>
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
