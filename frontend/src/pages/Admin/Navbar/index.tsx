fccd098df0d3c2e1e5788a301e4c88ff90f0197e
eliezer <eliezer.moraes@outlook.com>
2022-12-06 10:25:56 -0300

    Admin Page - Navbar HTML, layout

---
+++ frontend/src/pages/Admin/Navbar/index.tsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <p>Produtos</p>
+                </li>
+                <li>
+                    <p>Categorias</p>
+                </li>
+                <li>
+                    <p>Usuários</p>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,6 +1,14 @@
+import Navbar from "./Navbar"
+import './styles.css'
+
 const Admin = () => {
     return (
-        <h1>Página Admin</h1>
+        <div className="admin-container">
+            <Navbar />
+            <div className="admin-content">
+                <h1>Conteudo</h1>
+            </div>
+        </div>
     )
 }
 
