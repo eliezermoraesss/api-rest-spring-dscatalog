5d6074035f3cec5730ed9352e0999d2f9da8e838
eliezer <eliezer.moraes@outlook.com>
2022-12-07 15:05:24 -0300

    Axios, React Hooks, useState, useEffect

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,27 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css'
 
 const ProductDetails = () => {
+
+    const [product, setProduct] = useState<Product>()
+
+    useEffect(() => {
+        axios.get(BASE_URL + "/products/1")
+        .then(response => {
+            setProduct(response.data)
+        })
+    }, [])
+
+    // FORMA INCORRETA
+    //let product : Product;
+
     return (
         <div className="product-details-container">
             <div className="base-card product-details-card">
@@ -17,17 +34,17 @@ const ProductDetails = () => {
                 <div className="row">
                     <div className="col-xl-6">
                         <div className="img-container">
-                            <img src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg' alt='Nome do produto'></img>
+                            <img src={product?.imgUrl} alt={product?.name}></img>
                         </div>
                         <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={2345.67} />
+                            <h1>{product?.name}</h1>
+                            {product && <ProductPrice price={product?.price} />}
                         </div>
                     </div>
                     <div className="col-xl-6">
                         <div className="description-container">
                             <h2>Descrição do produto</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto, cum nostrum veniam ullam repellat quaerat vero voluptatem voluptate eligendi! Natus repudiandae exercitationem nisi ipsam suscipit commodi velit asperiores quos.</p>
+                            <p>{product?.description}</p>
                         </div>
                     </div>
                 </div>
