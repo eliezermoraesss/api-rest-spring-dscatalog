a7718d323ee230ab3c102b313b7e61a79b7510d1
eliezer <eliezer.moraes@outlook.com>
2022-12-07 16:03:35 -0300

    Hook, useParams

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -2,25 +2,28 @@ import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
 import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
 
 import './styles.css'
 
+type UrlParams = {
+    productId: string
+}
+
 const ProductDetails = () => {
+
+    const  { productId } = useParams<UrlParams>()
 
     const [product, setProduct] = useState<Product>()
 
     useEffect(() => {
-        axios.get(BASE_URL + "/products/1")
+        axios.get(`${BASE_URL}/products/${productId}`)
         .then(response => {
             setProduct(response.data)
         })
-    }, [])
-
-    // FORMA INCORRETA
-    //let product : Product;
+    }, [productId])
 
     return (
         <div className="product-details-container">
